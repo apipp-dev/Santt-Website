6bba81b87a3e6887885a1099dfbf2fab6036ba08
apipp <afifm192@gmail.com>
2026-06-23 15:00:34 +0700

    Delete components directory

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-transparent py-8">
-      <div className="container mx-auto px-4 text-center text-sm text-brand-softGray/80">
-        © {new Date().getFullYear()} SANTT PRODUCTION. All rights reserved.
-      </div>
-    </footer>
-  );
-}
